ca47d3c96db2f3cf6202f2b3d017b642220d7097
ST10091324 <ST10091324@vcconnect.edu.za>
2023-04-25 14:35:23 +0200

    Adding Class Project

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateCircleArea
+{
+    public class Circle
+    {
+        int radius;
+        int diameter;
+        double smallerCircleArea;
+        double biggerCircleArea;
+
+        public Circle(int smallerCircleRadius)
+        {
+            radius = smallerCircleRadius;
+        }
+
+        public double calculateSmallerCircleArae()
+        {
+            smallerCircleArea = Math.Pow(radius,2) * 3.14;
+
+            return smallerCircleArea;
+        }
+
+        public double calculateBiggerCircleArea()
+        {
+            biggerCircleArea = smallerCircleArea * 2;
+            return biggerCircleArea;
+        }
+
+        public double getSmallerCircleArea()
+        {
+            return smallerCircleArea;
+        }
+
+        public double getBiggerCircleArea()
+        {
+            return biggerCircleArea;
+        }
+
+        public void printCircleArea()
+        {
+            Console.WriteLine("Small circle Area: " + this.getSmallerCircleArea());
+            Console.WriteLine("Big circle Area: " + this.getBiggerCircleArea());
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter radius of smaller circle: ");
+            String circleRadius = Console.ReadLine();
+            Circle circleObj = new Circle(Convert.ToInt32(circleRadius));
+            circleObj.calculateSmallerCircleArae();
+            circleObj.calculateBiggerCircleArea();
+            circleObj.printCircleArea();
+            Console.ReadKey();
+        }
+    }
+}
